05676d064e05465f11c6dc270cfd67e238e600d5
Joe-noh <goflb.jh@gmail.com>
2013-08-17 13:24:15 +0900

    mistook order of enable session and require

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 ENV['RACK_ENV'] ||= 'development'
 
-enable :session
-
 require 'bundler/setup'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+enable :sessions
 
 Dir.glob("#{__dir__}/{config,models}/*.rb").each do |file|
   require file
