1ef72cec2ea7270e9366d38e5e5eb24e4725a3bc
Joe-noh <goflb.jh@gmail.com>
2013-08-17 10:17:18 +0900

    add warden strategy

--- config/warden.rb
+++ config/warden.rb
@@ -1,1 +1,30 @@
-# warden config
+Warden::Strategies.add :custom_auth_strategy do
+  def valid?
+    params['username'] && params['password']
+  end
+
+  def authenticate!
+    user = User.where('name = ?', params['username']).first
+    fail! 'Authentication Failed.' unless user
+
+    if user.digest == SCrypt::Engine.hash_secret(params['password'], user.salt)
+      success! user
+    else
+      fail! 'Authentication Failed.'
+    end
+  end
+end
+
+Warden::Manager.serialize_into_session do |user|
+  user.name
+end
+
+Warden::Manager.serialize_from_session do |name|
+  User.where('name = ?', name).first
+end
+
+helpers do
+  def warden
+    env['warden']
+  end
+end
