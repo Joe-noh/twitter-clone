acd6ceea11fdaf8517a5c4de5b0bcf2f722d74c3
Joe-noh <goflb.jh@gmail.com>
2013-08-17 12:55:29 +0900

    draft app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,60 @@
 
 get '/' do
-  'Hello'
+  if logged_in?
+    slim :home
+  else
+    slim :login
+  end
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  warden.authenticate!
+  redirect '/'
+end
+
+post '/unauthenticated' do
+  @error = warden.error
+  slim :login
+end
+
+get '/logout' do
+  warden.logout
+  slim :login
+end
+
+
+get '/user/new' do
+  'registration form'
+end
+
+post '/user/new' do
+  'make new user'
+end
+
+
+get '/user/profile/:name' do
+  'user profile page'
+end
+
+get '/user/profile/:name/edit' do
+  'confirm that :name is warden.user.name'
+  'edit form'
+end
+
+post '/user/profile/:name/edit' do
+  'confirm that :name is warden.user.name'
+  'edit user profile'
+end
+
+
+post '/status/new' do
+  'make new status'
+end
+
+get '/status/:id' do
+  'show the status'
 end
